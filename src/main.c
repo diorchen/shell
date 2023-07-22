4f2703c99ac80d59e5fd3f66c2829b709d1f8d74
Dior Chen <diorchen@Diors-MacBook-Air.local>
2023-07-21 22:36:47 -0700

    Revised comments

--- src/main.c
+++ src/main.c
@@ -36,11 +36,6 @@ int lsh_num_builtins() {
   Builtin function implementations.
 */
 
-/**
-   @brief Builtin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
- */
 int lsh_cd(char **args)
 {
   if (args[1] == NULL) { // no directory argument was provided 
@@ -53,11 +48,6 @@ int lsh_cd(char **args)
   return 1;
 }
 
-/**
-   @brief Builtin command: print help.
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
- */
 int lsh_help(char **args)
 {
   int i;
@@ -73,21 +63,11 @@ int lsh_help(char **args)
   return 1;
 }
 
-/**
-   @brief Builtin command: exit.
-   @param args List of args.  Not examined.
-   @return Always returns 0, to terminate execution.
- */
 int lsh_exit(char **args)
 {
   return 0;
 }
 
-/**
-  @brief Launch a program and wait for it to terminate.
-  @param args Null terminated list of arguments (including program).
-  @return Always returns 1, to continue execution.
- */
 int lsh_launch(char **args)
 {
   pid_t pid; // stores process ID of child process 
@@ -110,11 +90,6 @@ int lsh_launch(char **args)
   return 1;
 }
 
-/**
-   @brief Execute shell built-in or launch program.
-   @param args Null terminated list of arguments.
-   @return 1 if the shell should continue running, 0 if it should terminate
- */
 int lsh_execute(char **args)
 {
   int i;
@@ -132,10 +107,6 @@ int lsh_execute(char **args)
   return lsh_launch(args);
 }
 
-/**
-   @brief Read a line of input from stdin.
-   @return The line from stdin.
- */
 char *lsh_read_line(void)
 {
 #ifdef LSH_USE_STD_GETLINE // read line of input from standard input stdin
@@ -189,11 +160,7 @@ char *lsh_read_line(void)
 
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
-/**
-   @brief Split a line into tokens (very naively).
-   @param line The line.
-   @return Null-terminated array of tokens.
- */
+
 char **lsh_split_line(char *line)
 {
   int bufsize = LSH_TOK_BUFSIZE, position = 0;
@@ -227,9 +194,7 @@ char **lsh_split_line(char *line)
   return tokens;
 }
 
-/**
-   @brief Loop getting input and executing it.
- */
+
 void lsh_loop(void)
 {
   char *line;
@@ -247,20 +212,11 @@ void lsh_loop(void)
   } while (status);
 }
 
-/**
-   @brief Main entry point.
-   @param argc Argument count.
-   @param argv Argument vector.
-   @return status code
- */
 int main(int argc, char **argv)
 {
-  // Load config files, if any.
 
-  // Run command loop.
   lsh_loop();
 
-  // Perform any shutdown/cleanup.
 
   return EXIT_SUCCESS;
 }
